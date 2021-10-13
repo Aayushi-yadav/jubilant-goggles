858a6dc230ab7ede2af275a42de46369bb3e3c75
Aayushi-yadav <65588322+Aayushi-yadav@users.noreply.github.com>
2021-10-14 00:47:42 +0530

    Add files via upload

---
+++ src/main/java/com/assignment/odiSoft/FileUploadApplication.java
@@ -0,0 +1,18 @@
+package com.assignment.odiSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.assignment.odiSoft.property.FileUploadProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+		FileUploadProperties.class
+})
+public class FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadApplication.class, args);
+	}
+}
